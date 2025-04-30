77e30e31374959337fe4e9fd0a27c5fb9ab1f028
francielle oliveira da luz <francielleoliveiradaluz@Airdefrancielle.Princess.local>
2025-04-30 05:36:38 -0300

    inicio clean arch

--- src/main/java/br/com/alura/codechella/naousar/repository/repository/UsuarioRepository.java
+++ src/main/java/br/com/alura/codechella/naousar/repository/repository/UsuarioRepository.java
@@ -1,6 +1,6 @@
-package br.com.alura.codechella.repository;
+package br.com.alura.codechella.naousar.repository.repository;
 
-import br.com.alura.codechella.model.Usuario;
+import br.com.alura.codechella.naousar.model.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
--- src/main/java/br/com/alura/codechella/naousar/service/service/UsuarioService.java
+++ src/main/java/br/com/alura/codechella/naousar/service/service/UsuarioService.java
@@ -1,6 +1,6 @@
-package br.com.alura.codechella.service;
+package br.com.alura.codechella.naousar.service.service;
 
-import br.com.alura.codechella.model.Usuario;
+import br.com.alura.codechella.naousar.model.model.Usuario;
 
 import java.util.List;
 
